0bdfd4cc3a85f1bcb617e42a21ab9776309938b8
Mohasin Haque <mohasinhaque23121@gmail.com>
2022-03-01 12:30:38 +0530

    Done Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  
+  const [counter, setCounter] = useState(10);
+  const inputHandler = (e) => {
+    setCounter(10 - e.target.value.length)
+  }
+
   return (
     <div className="App">
       <h1>Fixed Input</h1>
+      <p>
+        ex1: character counter (Twitter)# See the Twitter post component. It
+        gives you an indicator of the character count. Replicate that behavior.
+      </p>
       <input onChange={inputHandler} placeholder="Enter Here" />
-      <h2>{remainingWords}</h2>
+      <h2>{counter}</h2>
     </div>
   );
 }
